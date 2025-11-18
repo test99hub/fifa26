8ff2943911d9cdf2cf0c428955d01a4d23d44a48
test99hub <test99hub@gmail.com>
2025-11-18 15:28:08 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = 'https://wqtvjnkixggpswqvyuaq.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxdHZqbmtpeGdncHN3cXZ5dWFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE4MzMwNzEsImV4cCI6MjA0NzQwOTA3MX0.xYlWkxJQOkXjYxfV5cQZOdL7gN6mD-0K8L3u5pXH2I4';
+const SUPABASE_URL = 'https://edtkpiybplwcnxerixem.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkdGtwaXlicGx3Y254ZXJpeGVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzODEzNDYsImV4cCI6MjA3ODk1NzM0Nn0.Q7Eb_4uGfwUXZAUUbDW-hcA-DARHqtwh1cfQ4ynofow';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
